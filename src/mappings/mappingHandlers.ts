24a78f6d25d366ed2a68d3d67b2025150973e465
lamafab <42901763+lamafab@users.noreply.github.com>
2022-03-22 17:01:15 +0100

    complete mappings of extrinsic

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,15 +1,48 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {Balance} from "@polkadot/types/interfaces";
+import { SubstrateExtrinsic, SubstrateEvent, SubstrateBlock } from "@subql/types";
+import { Extrinsic, FunctionArgument, SilField } from "../types";
+import { Balance } from "@polkadot/types/interfaces";
+import { Text, Compact, u32 } from "@polkadot/types-codec";
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const e = extrinsic.extrinsic;
-    logger.debug("---");
-    logger.debug(`toHuman: ${e.toHuman()}`);
-    logger.debug(`toJson: ${e.toJSON()}`);
-    logger.debug(`toString: ${e.toString()}`);
-    logger.debug(`inspect.name: ${e.inspect().name}`);
-    logger.debug(`method: ${e.method}`);
-    logger.debug(`meta: ${e.meta}`);
+    const e = new Extrinsic("");
+    const m = extrinsic.extrinsic.meta;
+    e.name = (m.name as Text).toHuman();
+
+    var ids = new Array();
+    for (let item of m.fields) {
+        const field = new SilField("");
+
+        if (item.name.isSome) {
+            field.name = (item.name.value as Text).toString();
+        }
+        field.type = (item.type as Compact<u32>).toNumber();
+        if (item.typeName.isSome) {
+            field.typeName = (item.typeName.value as Text).toString();
+        }
+        field.docs = (item.docs as Array<Text>).map((t) => { return t.toString(); });
+
+        await field.save();
+        ids.push(field.id);
+    }
+
+    e.fieldsId = ids;
+    e.index = m.index.toNumber();
+    e.docs = (m.docs as Array<Text>).map((t) => { return t.toString(); });
+
+    var ids = new Array();
+    for (let item of m.args) {
+        const arg = new FunctionArgument("");
+
+        arg.name = (item.name as Text).toString();
+        arg.type = (item.type as Text).toString();
+        if (item.typeName.isSome) {
+            arg.typeName = (item.typeName.value as Text).toString();
+        }
+
+        await arg.save();
+        ids.push(arg.id);
+    }
+
+    e.argsId = ids;
+    await e.save();
 }
-
-
