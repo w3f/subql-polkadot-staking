d87bf1dade8874ff1827a5ec4b813e7d02a9bb33
Nexus2k <philipp@web3.foundation>
2022-07-19 15:29:40 +0200

    Index set_controller and set_bond calls for controller list

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export * from './mappings/Extrinsics'
 //Rewards and Staking
 export * from "./mappings/Reward";
 export * from "./mappings/Staking";
+export * from "./mappings/SetController";
 //Transfers and BatchCalls
 export * from "./mappings/AccountTransfers";
 export * from "./mappings/BatchCalls";
-
---
+++ src/mappings/SetController.ts
@@ -0,0 +1,34 @@
+//https://github.com/subquery/tutorials-entity-relations/blob/main/src/mappings/AccountTransfers.ts
+import { SubstrateExtrinsic } from '@subql/types';
+import { Account } from '../types/models/Account';
+import { SetController } from '../types/models/SetController';
+
+async function ensureAccounts(accountIds: string[]): Promise<void> {
+    for (const accountId of accountIds) {
+        const account = await Account.get(accountId);
+        if (!account) {
+            await new Account(accountId).save();
+        }
+    }
+}
+
+export async function handleSetController(extrinsic: SubstrateExtrinsic): Promise<void> {
+    console.log(extrinsic)
+    const {
+        extrinsic: {
+            signer: id,
+            args: [args],
+        },
+    } = extrinsic;
+    var controller = extrinsic.extrinsic.args.toString();
+    // set_bond calls return 3 arguments... let's strip them away
+    if (controller.includes(",")) {
+        controller = controller.split(",")[0];
+    }
+    const setController = new SetController(
+        `${extrinsic.block.block.header.number.toNumber()}-${extrinsic.idx}`,
+    );
+    setController.stash = id.toString();
+    setController.controller = controller.toString();
+    await setController.save();
+}
